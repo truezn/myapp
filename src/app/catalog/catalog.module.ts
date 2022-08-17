eff9c56934ce8df4e67185982256e6a4c5085c11
Ning Zhang <ni.zhang@sap.com>
2022-08-17 16:12:31 +0800

    feat: add some comment for call AppRouingModule in CatalogModule

--- src/app/catalog/catalog.module.ts
+++ src/app/catalog/catalog.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CatalogRoutingModule } from './catalog-routing.module';
 import { CatalogComponent } from './catalog.component';
 import { SpartacusModule } from './spartacus/spartacus.module';
-// import { AppRoutingModule } from '@spartacus/storefront';
+// import { AppRoutingModule } from '@spartacus/storefront'; // if this is imported, then it will have forRoot() call twice issue.
 
 @NgModule({
   declarations: [CatalogComponent],
